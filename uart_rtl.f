21d607301ee3ab37d8546b3d866a24c534415a60
Muhammad Sufiyan Sadiq <sufiyansadiq1000@gmail.com>
2026-08-01 15:39:40 +0500

    added .f file for linting

---
+++ uart_rtl.f
@@ -0,0 +1,16 @@
++incdir+./packages
++incdir+./rtl
++incdir+./testbench
+
+./rtl/uart_params.svh
+./rtl/uart_interface.sv
+./rtl/sync_fifo.sv
+./rtl/register.sv
+./rtl/shift_register.sv
+./rtl/uart_rx_ctrl.sv
+./rtl/uart_tx_ctrl.sv
+./rtl/uart_rxtx_block.sv
+./rtl/ip_control_block.sv
+./rtl/uart_interrupt_control.sv
+./rtl/uart_16550_core.sv
+./rtl/uart_16550.sv
